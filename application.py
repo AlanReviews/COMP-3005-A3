4e1b90aeec855319c35eaf589a2627d4e9662857
Alan Zhou <alanzhou@cmail.carleton.ca>
2024-03-14 19:59:23 -0400

    Update application.py
    Added comments and new test code

--- application.py
+++ application.py
@@ -1,21 +1,26 @@
 import psycopg
 
+# Return the query that lists all the students in the database
 def getAllStudents(cursor):
     cursor.execute("SELECT * FROM students")
     return cursor.fetchall()
 
+# Add a student to the database
 def addStudent(cursor, first_name, last_name, email, enrollment_date):
     sql = "INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);"
     cursor.execute(sql, (first_name, last_name, email, enrollment_date))
 
+# Update the student's email given their ID
 def updateStudentEmail(cursor, student_id, new_email):
     updateStatement = "UPDATE students SET email = %s WHERE student_id = %s;"
     cursor.execute(updateStatement, (new_email, student_id))
 
+# Delete Student function removes the student from the database
 def deleteStudent(cursor, student_id):
     deleteStatement = "DELETE FROM students WHERE student_id = %s;"
     cursor.execute(deleteStatement, (student_id,))
 
+# Main function to connect to database and run the code
 def main():
     try:
         connection = psycopg.connect(
@@ -23,11 +28,11 @@ def main():
             "password=ab12345 host=localhost port=5432"
         )
         with connection.cursor() as cursor:
-            addStudent(cursor, "Alan", "Nguyen", "alannguyen@gmail.com", "2018-01-01")
-            updateStudentEmail(cursor, 1, "johndoe@email.com")
+            addStudent(cursor, "Jack", "Doe", "JackDoe@email.com", "2018-01-01")
+            updateStudentEmail(cursor, "1", "johndoe@email.com")
             results = getAllStudents(cursor)
             print(results)
-            deleteStudent(cursor, "1")
+            deleteStudent(cursor, "100")
             results = getAllStudents(cursor)
             print(results)
 
@@ -35,4 +40,5 @@ def main():
         print(f"error: {e}")
         exit(1)
 
+# Run the main function
 main()
