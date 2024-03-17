8b658aac0b1ff189caa5081c4a2b2e7066085b3a
Alan Zhou <alanzhou@cmail.carleton.ca>
2024-03-17 13:38:58 -0400

    Update application.py
    Added commit statements. Updated code.

--- application.py
+++ application.py
@@ -2,23 +2,26 @@ import psycopg
 
 # Return the query that lists all the students in the database
 def getAllStudents(cursor):
-    cursor.execute("SELECT * FROM students")
+    cursor.execute("SELECT * FROM students;")
     return cursor.fetchall()
 
 # Add a student to the database
 def addStudent(cursor, first_name, last_name, email, enrollment_date):
     sql = "INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);"
     cursor.execute(sql, (first_name, last_name, email, enrollment_date))
+    cursor.execute("COMMIT;")
 
 # Update the student's email given their ID
 def updateStudentEmail(cursor, student_id, new_email):
     updateStatement = "UPDATE students SET email = %s WHERE student_id = %s;"
     cursor.execute(updateStatement, (new_email, student_id))
+    cursor.execute("COMMIT;")
 
 # Delete Student function removes the student from the database
 def deleteStudent(cursor, student_id):
     deleteStatement = "DELETE FROM students WHERE student_id = %s;"
     cursor.execute(deleteStatement, (student_id,))
+    cursor.execute("COMMIT;")
 
 # Main function to connect to database and run the code
 def main():
@@ -28,11 +31,11 @@ def main():
             "password=ab12345 host=localhost port=5432"
         )
         with connection.cursor() as cursor:
-            addStudent(cursor, "Jack", "Doe", "JackDoe@email.com", "2018-01-01")
+            addStudent(cursor, "Josh", "Doe", "JoshDoe@email.com", "2025-01-01")
             updateStudentEmail(cursor, "1", "johndoe@email.com")
             results = getAllStudents(cursor)
             print(results)
-            deleteStudent(cursor, "100")
+            deleteStudent(cursor, "27")
             results = getAllStudents(cursor)
             print(results)
 
